6888b2d0610b161bd4dfcd82436a9dc74003d659
Juan Cantero Jarilla <juan.cantero.jarilla@gmail.com>
2021-02-05 17:40:15 +0100

    tutorial acabado, falta parte de borrado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { todo } from './todos.json';
+import TodoForm from './components/TodoForm';
 
 class App extends Component{
 
@@ -9,7 +10,14 @@ class App extends Component{
     super();
     this.state = { //Se configura el estado, que en este caso recogerá los valores del todos.json simulando una BBDD
       todo
-    }
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(todo){
+    this.setState({
+      todo: [...this.state.todo, todo] //Le concatenamos los nuevos datos que se vayan a añadir
+    })
   }
 
   render(){
@@ -17,27 +25,30 @@ class App extends Component{
       return(
         //Para cada uno de los elementos se creará un div que contendrá el atributo titulo de ese objeto
          <div className="col-md-4">
-           <div className="card mt-4"> 
-          <div className="card-header">
-            <h3>{ todo.title }</h3>
-            <span className="badge rounded-pill bg-danger ms-2">
-              { todo.priority }
-            </span>
-          </div>
-          <div className="card-body">
-            <p>{ todo.description }</p>
-            <mark>{ todo.responsible }</mark>
-          </div>
-        </div>
+            <div className="card mt-4"> 
+              <div className="card-header">
+                <h3>{ todo.title }</h3>
+                <h6>
+                  <span className="badge rounded-pill bg-danger">
+                    { todo.priority }
+                  </span>  
+                </h6>
+              </div>
+              
+              <div className="card-body">
+                <p>{ todo.description }</p>
+                <mark>{ todo.responsible }</mark>
+              </div>
+            </div>
          </div>
          
       )
-    })
+    });
 
     return (
       <div className="App">
         <nav className="navbar navbar-dark bg-dark">
-                <a href="" className="text-white">
+                <a href="/" className="text-white">
                     Tasks
                     <span className="badge rounded-pill bg-light text-dark ms-3">
                       { this.state.todo.length }
@@ -46,12 +57,17 @@ class App extends Component{
         </nav>
         <div className="container">
           <div className="row mt-4">
-          { todos }
+            <div className="col-md-3">
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+            </div>
+            <div className="col-md-8">
+              <div className="row">
+                { todos }
+              </div>
+            </div>                         
           </div>
         </div>
-         
-
-        <img src={logo} className="App-logo" alt="logo" /> 
+        
       </div>
     );
   }
